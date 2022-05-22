228489f0eb42ee4e47ac27a388dbf47caed0a78b
Fedor Donskov <fedya_donskov@mail.ru>
2022-05-22 16:35:45 +0300

    Создание stackView история.

--- CreateCodeProfileVK/ViewController.swift
+++ CreateCodeProfileVK/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
         let image = UIImageView()
         image.backgroundColor = .green
         
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = 50
+        
         image.translatesAutoresizingMaskIntoConstraints = false
         
         return image
@@ -51,8 +54,43 @@ class ViewController: UIViewController {
         return label
     }()
     
+    private lazy var editButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Редактировать", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        button.backgroundColor = .systemGreen
+        
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 18
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    // Функциональные кнопки
+    private lazy var logoButtonHistory: UIButton = {
+        let logoButton = UIButton()
+        logoButton.setImage(UIImage(named: "camera"), for: .normal)
+        
+        return logoButton
+    }()
+    
+    private lazy var labelHistory: UILabel = {
+        let labelHistory = UILabel()
+        labelHistory.font = .systemFont(ofSize: 15)
+        labelHistory.text = "История"
+        
+        labelHistory.translatesAutoresizingMaskIntoConstraints = false
+        
+        return labelHistory
+    }()
+        
+    
+    // MARK: - Elements stackView
+    
     private lazy var descriptionNameStatusStackView: UIStackView = {
-
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .equalSpacing
@@ -63,6 +101,20 @@ class ViewController: UIViewController {
         return stackView
     }()
     
+    private lazy var buttonHistoryStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 5
+                
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    
+    
     // MARK: - viewDidLoad
 
     override func viewDidLoad() {
@@ -71,10 +123,15 @@ class ViewController: UIViewController {
 
         view.addSubview(imageViewPhoto)
         view.addSubview(descriptionNameStatusStackView)
+        view.addSubview(editButton)
+        view.addSubview(buttonHistoryStackView)
         
         descriptionNameStatusStackView.addArrangedSubview(secondFirstName)
         descriptionNameStatusStackView.addArrangedSubview(createStatus)
         descriptionNameStatusStackView.addArrangedSubview(displayStatus)
+        
+        buttonHistoryStackView.addArrangedSubview(logoButtonHistory)
+        buttonHistoryStackView.addArrangedSubview(labelHistory)
         
         hierarhyConstraint()
     }
@@ -84,6 +141,10 @@ class ViewController: UIViewController {
     private func hierarhyConstraint() {
         imageViewPhotoConstraints()
         descriptionNameStatusConstraint()
+        
+        editButtonConstraint()
+        
+//        buttonConstraint()
     }
     
     // MARK: - Constraints elements
@@ -106,6 +167,21 @@ class ViewController: UIViewController {
         ])
     }
     
-
+    private func editButtonConstraint() {
+        NSLayoutConstraint.activate([
+            editButton.topAnchor.constraint(equalTo: imageViewPhoto.bottomAnchor, constant: 20),
+            editButton.leadingAnchor.constraint(equalTo: imageViewPhoto.leadingAnchor),
+            editButton.trailingAnchor.constraint(equalTo: descriptionNameStatusStackView.trailingAnchor),
+            editButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+//    private func buttonConstraint() {
+//        NSLayoutConstraint.activate([
+//            buttonHistoryStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            buttonHistoryStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//        ])
+//    }
+    
 }
 
